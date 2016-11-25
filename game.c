f055b7f948d9009bbe2f3999b3cb05ee7b3e2eda
Micheletti Julien <j.micheletti@hotmail.fr>
2016-11-25 12:16:37 +0100

    modif sprite + ajout de blocs amovible

--- game.c
+++ game.c
@@ -7,22 +7,32 @@
 #include "game.h"
 #include "level.h"
 
+void	moovebloc(int *first, int *second)
+{
+  if (*first == BLOC)
+    {
+      *second = BLOC;
+      *first = VIDE;
+    }
+}
+
 void	play(SDL_Surface *ecran)
 {
   SDL_Surface	*heros[4] = {NULL};
-  SDL_Surface	*mur = NULL, *herosActuel = NULL, *fin = NULL, *pacerelle = NULL;
+  SDL_Surface	*mur = NULL, *herosActuel = NULL, *fin = NULL, *bloc = NULL, *vide = NULL;
   SDL_Rect	pos, posPlayer;
   SDL_Event	event;
   int	continuer = 1, i = 0, j = 0, check = 0, select = 0;
-  int	now = 0, prev = 0;
   int	carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR] = {0};
 
   mur = IMG_Load("mur.png");
-  heros[BAS] = IMG_Load("heros_avant.gif");
-  heros[GAUCHE] = IMG_Load("heros_gauche.gif");
-  heros[HAUT] = IMG_Load("heros_arrière.gif");
-  heros[DROITE] = IMG_Load("heros_droite.gif");
+  bloc = IMG_Load("bloc.gif");
+  heros[BAS] = IMG_Load("Devant.png");
+  heros[GAUCHE] = IMG_Load("gauche.png");
+  heros[HAUT] = IMG_Load("derriere.png");
+  heros[DROITE] = IMG_Load("Droite.png");
   fin = IMG_Load("objectif.png");
+  vide = IMG_Load("sol.png");
 
   herosActuel = heros[BAS];
   if (!chargerNiveau(carte, select))
@@ -75,6 +85,11 @@ void	play(SDL_Surface *ecran)
 		  SDL_BlitSurface(fin, NULL, ecran, &pos);
 		  check = 1;
 		}
+	      if (carte[i][j] == BLOC)
+		SDL_BlitSurface(bloc, NULL, ecran, &pos);
+	      if (carte[i][j] == VIDE)
+		SDL_BlitSurface(vide, NULL, ecran, &pos);
+	      
       	    }
       	}
        if (!check)
@@ -109,6 +124,9 @@ void	moovePlayer(int carte[][NB_BLOCS_HAUTEUR], SDL_Rect *pos, int direction)
 	return;
       else if (carte[pos->x][pos->y - 1] == FIN)
 	carte[pos->x][pos->y - 1] = VIDE;
+      if (carte[pos->x][pos->y - 1] == BLOC && (pos->y - 2 < 0 || carte[pos->x][pos->y - 2] == MUR || carte[pos->x][pos->y - 2] == BLOC))
+	return;
+      moovebloc(&carte[pos->x][pos->y - 1], &carte[pos->x][pos->y - 2]);
       pos->y--;
       return;
     }
@@ -120,10 +138,13 @@ void	moovePlayer(int carte[][NB_BLOCS_HAUTEUR], SDL_Rect *pos, int direction)
 	return;
       else if (carte[pos->x][pos->y + 1] == FIN)
 	carte[pos->x][pos->y + 1] = VIDE;
+      if (carte[pos->x][pos->y + 1] == BLOC && (pos->y + 2 < 0 || carte[pos->x][pos->y + 2] == MUR || carte[pos->x][pos->y + 2] == BLOC))
+	return;
+      moovebloc(&carte[pos->x][pos->y + 1], &carte[pos->x][pos->y + 2]);
       pos->y++;
       return;
     }
-  if (direction == GAUCHE)
+  else if (direction == GAUCHE)
     {
       if (pos->x - 1 < 0)
 	return;
@@ -131,19 +152,23 @@ void	moovePlayer(int carte[][NB_BLOCS_HAUTEUR], SDL_Rect *pos, int direction)
         return;
       else if (carte[pos->x - 1][pos->y] == FIN)
 	carte[pos->x - 1][pos->y] = VIDE;
+      if (carte[pos->x - 1][pos->y] == BLOC && (pos->x - 2 < 0 || carte[pos->x - 2][pos->y] == MUR || carte[pos->x - 2][pos->y] == BLOC))
+	return;
+      moovebloc(&carte[pos->x - 1][pos->y], &carte[pos->x - 2][pos->y]);
       pos->x--;
       return;
     }
-  if (direction == DROITE)
+  else if (direction == DROITE)
     {
       if (pos->x + 1 > NB_BLOCS_LARGEUR)
 	return;
       else if (carte[pos->x + 1][pos->y] == MUR)
 	return;
       else if (carte[pos->x + 1][pos->y] == FIN)
-	{
-	  carte[pos->x + 1][pos->y] = VIDE;
-	}
+	carte[pos->x + 1][pos->y] = VIDE;
+      if (carte[pos->x + 1][pos->y] == BLOC && (pos->x + 2 < 0 || carte[pos->x + 2][pos->y] == MUR || carte[pos->x + 2][pos->y] == BLOC))
+	return;
+      moovebloc(&carte[pos->x + 1][pos->y], &carte[pos->x + 2][pos->y]);
       pos->x++;
       return;
     }
--- sources.c
+++ sources.c
@@ -11,8 +11,8 @@ FILE*	selectNiveau(FILE* fichier, int select)
 {
   if (select == 0)
     fichier = fopen("niveau.lvl", "r");
-  else if (select == 1)
-    fichier = fopen("niveau2.lvl", "r");
+  /* else if (select == 1) */
+  /*   fichier = fopen("niveau2.lvl", "r"); */
   return (fichier);
 }
 
@@ -53,3 +53,109 @@ int	chargerNiveau(int niveau[][NB_BLOCS_HAUTEUR], int select)
   fclose(fichier);
   return 1;
 }
+
+/* int	chargerNiveau(int niveau[][NB_BLOCS_HAUTEUR], int select) */
+/* { */
+/*   char	*tab; */
+/*   char	*temp; */
+/*   /\* char	*chaine; *\/ */
+/*   int	taille = 0; */
+/*   FILE*	fichier = NULL; */
+/*   int	i = 0, j = 0, k = 0; */
+
+/*   tab = (char*)malloc((10000) * sizeof(char *)); */
+/*   temp = tab; */
+/*   /\* chaine = (char*)malloc((NB_BLOCS_LARGEUR) * sizeof(char)); *\/ */
+/*   fichier = selectNiveau(fichier, select); */
+/*   if (fichier == NULL) */
+/*     exit(1); */
+/*   while (fgetc(fichier) != NULL) */
+/*     { */
+/*       temp[j] = (char*)malloc((strlen(chaine)) * sizeof(char)); */
+/*       strcpy(temp[j], chaine); */
+/*       j++; */
+/*     } */
+/*   /\* for (i = 0 ; i < NB_BLOCS_LARGEUR ; i++) *\/ */
+/*   /\*   { *\/ */
+/*   /\*     for (j = 0 ; j < NB_BLOCS_HAUTEUR ; j++) *\/ */
+/*   /\* 	{ *\/ */
+/*   /\* 	  if (tab[j][i] == '0') *\/ */
+/*   /\* 	    niveau[j][i] = 0; *\/ */
+/*   /\* 	  else if (tab[j][i] == '1') *\/ */
+/*   /\* 	      niveau[j][i] = 1; *\/ */
+/*   /\* 	  else if (tab[j][i] == '2') *\/ */
+/*   /\* 	      niveau[j][i] = 2; *\/ */
+/*   /\* 	  else if (tab[j][i] == '3') *\/ */
+/*   /\* 	      niveau[j][i] = 3; *\/ */
+/*   /\* 	  else if (tab[j][i] == '4') *\/ */
+/*   /\* 	    niveau[j][i] = 4; *\/ */
+/*   /\* 	} *\/ */
+/*   /\*   } *\/ */
+/* } */
+
+/*   /\* taille = j; *\/ */
+/*   /\* j = 0; *\/ */
+/*   /\* while (j < taille) *\/ */
+/*   /\*   { *\/ */
+/*   /\*     printf("%s", tab[j]); *\/ */
+/*   /\*     j++; *\/ */
+/*   /\*   } *\/ */
+
+
+/* 	  /\* if (j < NB_BLOCS_HAUTEUR) *\/ */
+/* 	  /\*   { *\/ */
+/* 	  /\*     for (i = 0 ; i < NB_BLOCS_LARGEUR ; i++) *\/ */
+/* 	  /\* 	{ *\/ */
+/* 	  /\* 	  if (chaine[i] == '0') *\/ */
+/* 	  /\* 	    niveau[j][i] = 0; *\/ */
+/* 	  /\* 	  else if (chaine[i] == '1') *\/ */
+/* 	  /\* 	    niveau[j][i] = 1; *\/ */
+/* 	  /\* 	  else if (chaine[i] == '2') *\/ */
+/* 	  /\* 	    niveau[j][i] = 2; *\/ */
+/* 	  /\* 	  else if (chaine[i] == '3') *\/ */
+/* 	  /\* 	    niveau[j][i] = 3; *\/ */
+/* 	  /\* 	  else if (chaine[i] == '4') *\/ */
+/* 	  /\* 	    niveau[j][i] = 4; *\/ */
+/* 	  /\* 	} *\/ */
+/* 	  /\*     j++; *\/ */
+
+/* /\* int	chargerNiveau(int niveau[][NB_BLOCS_HAUTEUR], int select) *\/ */
+/* /\* { *\/ */
+/* /\*   FILE*	fichier = NULL; *\/ */
+/* /\*   char	ligneFichier[NB_BLOCS_LARGEUR * NB_BLOCS_HAUTEUR + 1] = {0}; *\/ */
+/* /\*   int	i = 0, j = 0; *\/ */
+
+/* /\*   fichier = selectNiveau(fichier, select); *\/ */
+/* /\*   if (fichier == NULL) *\/ */
+/* /\*     { *\/ */
+/* /\*       printf("OUAIS\n"); *\/ */
+/* /\*       return 0; *\/ */
+/* /\*     } *\/ */
+/* /\*   fgets(ligneFichier, NB_BLOCS_LARGEUR * NB_BLOCS_HAUTEUR + 1, fichier); *\/ */
+/* /\*   for (i = 0 ; i < NB_BLOCS_LARGEUR ; i++) *\/ */
+/* /\*     { *\/ */
+/* /\*       for (j = 0 ; j < NB_BLOCS_HAUTEUR ; j++) *\/ */
+/* /\* 	{ *\/ */
+/* /\* 	  switch (ligneFichier[(i * NB_BLOCS_LARGEUR) + j]) *\/ */
+/* /\* 	    { *\/ */
+/* /\* 	    case '0': *\/ */
+/* /\* 	      niveau[j][i] = 0; *\/ */
+/* /\* 	      break; *\/ */
+/* /\* 	    case '1': *\/ */
+/* /\* 	      niveau[j][i] = 1; *\/ */
+/* /\* 	      break; *\/ */
+/* /\* 	    case '2': *\/ */
+/* /\* 	      niveau[j][i] = 2; *\/ */
+/* /\* 	      break; *\/ */
+/* /\* 	    case '3': *\/ */
+/* /\* 	      niveau[j][i] = 3; *\/ */
+/* /\* 	      break; *\/ */
+/* /\* 	    case '4': *\/ */
+/* /\* 	      niveau[j][i] = 4; *\/ */
+/* /\* 	      break; *\/ */
+/* /\* 	    } *\/ */
+/* /\* 	} *\/ */
+/* /\*     } *\/ */
+/* /\*   fclose(fichier); *\/ */
+/* /\*   return 1; *\/ */
+/* /\* } *\/ */
